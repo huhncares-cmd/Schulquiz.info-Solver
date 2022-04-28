ab590296bdfb51e24a70654b27c68ba1cc13b67c
huhncares <huhn.cares@gmx.de>
2022-04-28 21:50:56 +0200

    1.0.4

--- js/solve.js
+++ js/solve.js
@@ -1,12 +1,25 @@
-document.getElementById('anzahl_richtig').value = document.getElementById('anzahl_antworten').value;
-let inputs = document.querySelectorAll('input');
-for(let i = 0; i < inputs.length; i++) {
-    input = inputs[i];
-    if(input.value == "Überprüfen!") {
-        input.disabled = false;
-        input.click();
-    }
-    if(document.getElementById('anzahl_richtig').value > document.getElementById('anzahl_antworten').value) {
-        document.getElementById('anzahl_richtig').value = document.getElementById('anzahl_antworten').value;
+function setValueToMaximumPoints() {
+    document.getElementById('anzahl_richtig').value = document.getElementById('anzahl_antworten').value;
+}
+function clickAllInputs() {
+    let inputs = Array();
+    document.querySelectorAll('input').forEach(input => {
+        if(input.value == "Überprüfen!") {
+            inputs.push(input);
+        }
+    });
+    for(let i = 0; i < inputs.length; i++) {
+        input = inputs[i];
+        if(input.value == "Überprüfen!") {
+            input.disabled = false;
+            if(i < inputs.length - 1) {
+                input.click();
+            }
+            if(document.getElementById('anzahl_richtig').value > document.getElementById('anzahl_antworten').value) {
+                setValueToMaximumPoints();
+            }
+        }
     }
 }
+setValueToMaximumPoints();
+clickAllInputs();
